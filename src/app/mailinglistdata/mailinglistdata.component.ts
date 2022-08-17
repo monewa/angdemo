9229cd2bb8dd222d7206993e7e84943ab5854e8e
monewa <107672359+monewa@users.noreply.github.com>
2022-08-17 07:49:53 +0200

    Add files via upload

--- src/app/mailinglistdata/mailinglistdata.component.ts
+++ src/app/mailinglistdata/mailinglistdata.component.ts
@@ -11,13 +11,17 @@ import { WindowService } from '../services/window.service';
 export class MailinglistdataComponent implements OnInit {
 
 	users: any[]= this.repository.users;
+	messageLog: string= '';
+	eventLogIsOpen= false;
 	message: string= '';
+	messageBoxIsOpen= false;
 	selectedIndex: number= -1;
 	name: string= '';
 	lastName: string= '';
 	country: string= '';
 	email: string= '';
 	phone: string= '';
+	messageColor= ''
 	timeout= setTimeout(() => {this.users= this.repository.users;}, 4000);
 	
 	constructor(private repository:UserRepositoryService, private window:WindowService) { }
@@ -40,18 +44,21 @@ export class MailinglistdataComponent implements OnInit {
 	
 	updateData(index:number): void{
 		this.selectedIndex= -1;
-		this.message = `id no:${this.repository.getId(index)} was selected<br>`;
+		this.messageLog += `<br>id no:${this.repository.getId(index)} was selected<br>`;
+		this.message= `id no:${this.repository.getId(index)} was edited`;
+		this.messageColor= 'background-color:rgb(0, 81, 255)';
 		this.updateName(index);
-		this.updateLastName(index);
+		this.updateLastName(index); 
 		this.updateCountry(index);
 		this.updateEmail(index);
 		this.updatePhone(index);
 		this.users = this.repository.users;
+		this.openMessageBox();
 	}
 
 	updateName(index:number): void{
 		if(this.repository.getFirstName(index) != this.name){
-			this.message+= ` ${this.repository.getFirstName(index)} is changed to ${this.name}<br>`;
+			this.messageLog+= ` ${this.repository.getFirstName(index)} is changed to ${this.name}<br>`;
 			this.repository.patchName(index, this.name);
 
 		}
@@ -59,42 +66,81 @@ export class MailinglistdataComponent implements OnInit {
 	
 	updateLastName(index:number): void{
 		if(this.repository.getLastName(index) != this.lastName){
-			this.message+= `${this.repository.getLastName(index)} is changed to ${this.lastName}<br>`;
+			this.messageLog+= `${this.repository.getLastName(index)} is changed to ${this.lastName}<br>`;
 			this.repository.patchLastname(index, this.lastName);
 		}
 	}
 	
 	updateCountry(index:number): void{
 		if(this.repository.getCountry(index) != this.country){
-			this.message+= `${this.repository.getCountry(index)} is changed to ${this.country}<br>`;
+			this.messageLog+= `${this.repository.getCountry(index)} is changed to ${this.country}<br>`;
 			this.repository.patchCounty(index, this.country);
 		}
 	}
 	
 	updateEmail(index:number): void{
 		if(this.repository.getEmail(index) != this.email){
-			this.message+= `${this.repository.getEmail(index)} is changed to ${this.email}<br>`;
+			this.messageLog+= `${this.repository.getEmail(index)} is changed to ${this.email}<br>`;
 			this.repository.patchEmail(index, this.email);
 		}
 	}
 	
 	updatePhone(index:number): void{
 		if(this.repository.getPhone(index) != this.phone){
-			this.message+= `${this.repository.getPhone(index)} is changed to ${this.phone}<br>`;
+			this.messageLog+= `${this.repository.getPhone(index)} is changed to ${this.phone}<br>`;
 			this.repository.patchPhone(index, this.phone);
 		}
 	}
 	
 	deleteUser(index:number): void{
-		this.message= `id no:${this.repository.getId(index)} ${this.repository.getFirstName(index)}  is deleted`;
+		this.messageLog+= `<br>id no:${this.repository.getId(index)} ${this.repository.getFirstName(index)}  is deleted<br>`;
+		this.messageColor= 'background-color: crimson';
+		this.message= `id no:${this.repository.getId(index)} was deleted`;
 		this.repository.deleteUser(index);
-		this.users= this.repository.users
+		this.users= this.repository.users;
+	}
+
+	openMessageBox():void{
+		this.messageBoxIsOpen= this.message== ''? false: true;
+		this.closeEventLog();
+	}
+
+	closeMessageBox():void{
+		this.messageBoxIsOpen= false;
+		this.message= "";
+	}
+
+	openEventLog(){
+		this.eventLogIsOpen= true;
+		this.closeMessageBox();
+
+	}
+
+	closeEventLog(){
+		this.eventLogIsOpen= false;
+	}
+
+	checkForEvents(){
+		if(this.messageLog==''){
+			return true;
+		}
+		return false;
+	}
+
+
+	closePopups(){
+		if(this.eventLogIsOpen){
+			this.closeEventLog();
+		}
+		if(this.messageBoxIsOpen){
+			this.closeMessageBox();
+		}
 	}
 
 	setWidth(length:number): string{
 		return `width:${length*5+ 80}px; font-size: 19px;`;
-	} 
-	
+	} 	
+
 	checkForData(): boolean{
 		if(this.users.length <= 0){
 			return false;
--- src/app/model/user.repository.service.ts
+++ src/app/model/user.repository.service.ts
@@ -12,19 +12,19 @@ export class UserRepositoryService {
 	
 	users: any[]= [];
 	countries: any[]= [];
-	// users:any[]= this.getStaticUserData();
-	//USERSURL:string= 'https://monewa.github.io/json-server/maildata.json';
-	USERSURL: string= 'assets/data/users.json';
-	// USERSURL:string= 'http://localhost:3500/users/';
-	// COUNTRIESURL:string= 'http://localhost:3500/countries-codes/';
-	COUNTRIESURL: string= 'assets/data/countrydata.json';
-	
+	SERVER1= 'https://mo-json-api.herokuapp.com'
+	SERVER2= 'http://localhost:3000'
+	USERFILE=  'assets/data/users.json'
+	// COUNTRYFILE=  'assets/data/countrydata.json'
+	// USERSURL: string= this.USERFILE;
+	USERSURL:string= `${this.SERVER1}/users`;
+	COUNTRIESURL:string= `${this.SERVER1}/countries-codes/`;
 	
 	constructor( private http:HttpClient) { 
 		this.get(); 
 		this.getCountries();
 	}
-	
+
 	getCountries(){
 		this.http.get<any[]>(this.COUNTRIESURL, {responseType: 'json'}).subscribe(
 			(data:any[])=> { this.countries= data; }
@@ -32,8 +32,7 @@ export class UserRepositoryService {
 	}
 
 	getFromServer(): Observable<User[]>{
-		return this.http.get<User[]>(this.USERSURL, {responseType: 'json'})
-			
+		return this.http.get<User[]>(this.USERSURL, {responseType: 'json'})	
 	}
 
 	get(){
@@ -54,7 +53,7 @@ export class UserRepositoryService {
 			"phone": phone,
 			"comments": comments
 		}
-		// this.http.post<User[]>(this.USERSURL, user,	{responseType: 'json'}).subscribe();
+		this.http.post<User[]>(this.USERSURL, user,	{responseType: 'json'}).subscribe();
 			this.users.push(user);
 			// this.get()
 	}
@@ -73,38 +72,53 @@ export class UserRepositoryService {
 	}
 
 	patchName(index: number, firstName: string): void{
-		// this.http.patch<User[]>(`${this.USERSURL}/${this.getId(index)}`, {"firstName": firstName}, {responseType: 'json'}).subscribe()
-		this.users[index].firstName= firstName
+		this.http.patch<User[]>(`${this.USERSURL}/${this.getId(index)}`, {"firstName": firstName}, {responseType: 'json'}).subscribe(
+			update=>{
+				this.users[index].firstName= firstName
+			}
+		)
 		// this.get()
 	}
 
 	patchLastname(index: number, lastName: string): void{
-		// this.http.patch<User[]>(`${this.USERSURL}/${this.getId(index)}`,  {"lastName": lastName}, {responseType: 'json'}).subscribe();
-		this.users[index].lastName= lastName
+		this.http.patch<User[]>(`${this.USERSURL}/${this.getId(index)}`,  {"lastName": lastName}, {responseType: 'json'}).subscribe(
+			update=>{
+				this.users[index].lastName= lastName
+			}
+		);
 		// this.get();
 	}
 
 	patchCounty(index: number, country: string): void{
-		// this.http.patch<User[]>(`${this.USERSURL}/${this.getId(index)}`, {"country": country}, {responseType: 'json'}).subscribe()
-		this.users[index].country= country
+		this.http.patch<User[]>(`${this.USERSURL}/${this.getId(index)}`, {"country": country}, {responseType: 'json'}).subscribe(
+			update=>{
+				this.users[index].country= country
+			}
+		)
 		// this.get()
 	}
 
 	patchEmail(index: number, email: string):void{
-		// this.http.patch<User[]>(`${this.USERSURL}/${this.getId(index)}`, {"email": email}, {responseType: 'json'}).subscribe();
+		this.http.patch<User[]>(`${this.USERSURL}/${this.getId(index)}`, {"email": email}, {responseType: 'json'}).subscribe();
 		this.users[index].email= email
 		// this.get()
 	}
 
 	patchPhone(index: number, phone: string):void{
-		// this.http.patch<User[]>(`${this.USERSURL}/${this.getId(index)}`, {"phone": phone}, {responseType: 'json'}).subscribe();
-		this.users[index].phone= phone
+		this.http.patch<User[]>(`${this.USERSURL}/${this.getId(index)}`, {"phone": phone}, {responseType: 'json'}).subscribe(
+			update=>{
+				this.users[index].phone= phone
+			}
+		);
 		// this.get()
 	}
 
 	deleteUser(index: number):void{
-		// this.http.delete<User[]>(`${this.USERSURL}/${this.getId(index)}`, {responseType: 'json'}).subscribe();
-		this.users.splice(index, 1)	
+		this.http.delete<User[]>(`${this.USERSURL}/${this.getId(index)}`, {responseType: 'json'}).subscribe(
+			deleted=>{
+				this.users.splice(index, 1)	
+			}
+		);
 	//	this.get()
 	}
 	
@@ -135,10 +149,5 @@ export class UserRepositoryService {
  	getComments(index: number):string{
 		return this.users[index].comments
 	}
-
-	getStaticUserData():any[]{
-	return [{"id":2,"firstName":"Thoho","lastName":"Yandou","country":"RSA","email":"thoho@gmail.com","phone":"+2783 4338670","comments":"like it"},{"id":3,"firstName":"Mary","lastName":"Ann","country":"UK","email":"mary@gmail.com","phone":"+5672 1450697","comments":"love it"},{"id":4,"firstName":"Jane","lastName":"Doring","country":"Australia","email":"jane@gmail.com","phone":"+5674 1434697","comments":"can't wait"},{"id":5,"firstName":"Franco","lastName":"Leon","country":"France","email":"franco@yahoo.com","phone":"+3472 1646334","comments":""},{"id":6,"firstName":"Mario","lastName":"Gotti","country":"Italy","email":"mario@gmail.com","phone":"+3378 8646864","comments":"like it"},{"id":7,"firstName":"Karima","lastName":"Singh","country":"India","email":"karima@gmail.com","phone":"+8370 0664063","comments":"excellent"},{"id":8,"firstName":"Xiao","lastName":"Wang","country":"China","email":"jing@weibo.com","phone":"+6785 0649651","comments":"good read"},{"id":9,"firstName":"Mebo","lastName":"Kamkwamba","country":"Malawi","email":"mebo@yahoo.com","phone":"+9795 4564961","comments":"good work"},{"id":10,"firstName":"Kudjo","lastName":"Omoso","country":"Nigeria","email":"kudjo@yahoo.com","phone":"+9575 4949610","comments":"love this author"}]
-	}
-
 
 }
