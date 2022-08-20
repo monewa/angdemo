4eaae39e4cc5ae31696c37f4b47baae8f92cc9a1
monewa <107672359+monewa@users.noreply.github.com>
2022-08-20 19:07:41 +0200

    Add files via upload

--- src/app/services/window.service.ts
+++ src/app/services/window.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class WindowService {
 
+  windowIsFrozen:boolean= false;
+
   constructor() { }
 
   scrollToTop(){
@@ -23,5 +25,14 @@ export class WindowService {
 			behavior: 'smooth'
 		  });
   }
+
+  freezeWindow(){
+      this.windowIsFrozen= true;
+  }
+
+  unfreezeWindow(){
+    this.windowIsFrozen= false;
+}
+
 
 }
