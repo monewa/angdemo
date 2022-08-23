909bc772e17c160e81ddee67346c2eb62cf52218
monewa <107672359+monewa@users.noreply.github.com>
2022-08-23 17:27:38 +0200

    Add files via upload

--- src/app/mailinglistdata/mailinglistdata.component.ts
+++ src/app/mailinglistdata/mailinglistdata.component.ts
@@ -12,9 +12,10 @@ export class MailinglistdataComponent implements OnInit {
 
 	users: any[]= this.repository.users;
 	messageLog: string= '';
-	eventLogIsOpen= false;
-	message: string= '';
-	messageBoxIsOpen= false;
+	eventLogIsOpen: boolean= false;
+	deleteNotificationIsOpen: boolean= false;
+	editNotificationIsOpen: boolean= false;
+	id: number= -1;
 
 	selectedIndex: number= -1;
 	name: string= '';
@@ -22,7 +23,6 @@ export class MailinglistdataComponent implements OnInit {
 	country: string= '';
 	email: string= '';
 	phone: string= '';
-	messageColor= ''
 	timeout= setTimeout(() => {this.users= this.repository.users;}, 4000);
 	
 	constructor(private repository:UserRepositoryService, private window:WindowService) { }
@@ -45,82 +45,85 @@ export class MailinglistdataComponent implements OnInit {
 	
 	updateData(index:number): void{
 		this.selectedIndex= -1;
-		this.messageLog += `<br>id no:${this.repository.getId(index)} was selected<br>`;
-		this.message= `id no:${this.repository.getId(index)} was edited`;
-		this.messageColor= 'background-color:rgb(0, 81, 255)';
+		this.messageLog += `<br>id no:${this.repository.getId(index)} ${this.repository.getFirstName(index)} was selected on <i>${new Date().toUTCString()}</i><br>`;
+		this.id= this.repository.getId(index);
 		this.updateName(index);
 		this.updateLastName(index); 
 		this.updateCountry(index);
 		this.updateEmail(index);
 		this.updatePhone(index);
 		this.users = this.repository.users;
-		this.openMessageBox();
+		this.openEditBox();
 	}
 
 	updateName(index:number): void{
 		if(this.repository.getFirstName(index) != this.name){
-			this.messageLog+= ` ${this.repository.getFirstName(index)} is changed to ${this.name}<br>`;
+			this.messageLog+= `- ${this.repository.getFirstName(index)} is changed to ${this.name}<br>`;
 			this.repository.patchName(index, this.name);
-
 		}
 	}
 	
 	updateLastName(index:number): void{
 		if(this.repository.getLastName(index) != this.lastName){
-			this.messageLog+= `${this.repository.getLastName(index)} is changed to ${this.lastName}<br>`;
+			this.messageLog+= `- ${this.repository.getLastName(index)} is changed to ${this.lastName}<br>`;
 			this.repository.patchLastname(index, this.lastName);
 		}
 	}
 	
 	updateCountry(index:number): void{
 		if(this.repository.getCountry(index) != this.country){
-			this.messageLog+= `${this.repository.getCountry(index)} is changed to ${this.country}<br>`;
+			this.messageLog+= `- ${this.repository.getCountry(index)} is changed to ${this.country}<br>`;
 			this.repository.patchCounty(index, this.country);
 		}
 	}
 	
 	updateEmail(index:number): void{
 		if(this.repository.getEmail(index) != this.email){
-			this.messageLog+= `${this.repository.getEmail(index)} is changed to ${this.email}<br>`;
+			this.messageLog+= `- ${this.repository.getEmail(index)} is changed to ${this.email}<br>`;
 			this.repository.patchEmail(index, this.email);
 		}
 	}
 	
 	updatePhone(index:number): void{
 		if(this.repository.getPhone(index) != this.phone){
-			this.messageLog+= `${this.repository.getPhone(index)} is changed to ${this.phone}<br>`;
+			this.messageLog+= `- ${this.repository.getPhone(index)} is changed to ${this.phone}<br>`;
 			this.repository.patchPhone(index, this.phone);
 		}
 	}
 	
 	deleteUser(index:number): void{
-		this.messageLog+= `<br>id no:${this.repository.getId(index)} ${this.repository.getFirstName(index)}  is deleted<br>`;
-		this.messageColor= 'background-color: crimson';
-		this.message= `id no:${this.repository.getId(index)} was deleted`;
+		this.messageLog+= `<br>id no:${this.repository.getId(index)} ${this.repository.getFirstName(index)}  was deleted on <i>${new Date().toUTCString()}</i><br>`;
+		this.id= this.repository.getId(index);
 		this.repository.deleteUser(index);
 		this.users= this.repository.users;
-		this.openMessageBox();
+		this.openDeleteBox();
 	}
 
-	openMessageBox():void{
-		this.messageBoxIsOpen= this.message== ''? false: true;
+	openEditBox():void{
+		this.editNotificationIsOpen= true;
 		this.closeEventLog();
 	}
 
-	closeMessageBox():void{
-		this.messageBoxIsOpen= false;
-		this.message= "";
+	closeEditBox():void{
+		this.editNotificationIsOpen= false;
+	}
+
+	openDeleteBox():void{
+		this.deleteNotificationIsOpen= true;
+		this.closeEventLog();
+	}
+
+	closeDeleteBox():void{
+		this.deleteNotificationIsOpen= false;
 	}
 
 	openEventLog(){
 		this.eventLogIsOpen= true;
-		this.closeMessageBox();
-		this.window.freezeWindow();
 	}
 
 	closeEventLog(){
 		this.eventLogIsOpen= false;
-		this.window.unfreezeWindow();
+		// this.window.unfreezeWindow();
 	}
 
 	checkForEvents(){
@@ -131,12 +134,9 @@ export class MailinglistdataComponent implements OnInit {
 	}
 
 	closePopups(){
-		if(this.eventLogIsOpen){
-			this.closeEventLog();
-		}
-		if(this.messageBoxIsOpen){
-			this.closeMessageBox();
-		}
+		this.closeEventLog();
+		this.closeEditBox();
+		this.closeDeleteBox();
 	}
 
 	setWidth(length:number): string{
