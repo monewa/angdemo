374ccfb1f1b872d479e450d28d5e24d5714c792c
monewa <107672359+monewa@users.noreply.github.com>
2022-08-29 13:12:40 +0200

    Add files via upload

--- src/app/bookstore/bookstore.component.ts
+++ src/app/bookstore/bookstore.component.ts
@@ -1,19 +1,23 @@
 
 import { Component, OnInit } from '@angular/core';
 import { WindowService } from '../services/window.service';
+import { UserRepositoryService } from '../model/user.repository.service';
 
 @Component({
   selector: 'app-bookstore',
   templateUrl: './bookstore.component.html',
-  styleUrls: ['./bookstore.component.css', '../app.component.css', '../mailinglist/mailinglist.component.css']
+  styleUrls: ['./bookstore.component.css', '../app.component.css']
 })
 export class BookstoreComponent implements OnInit {
 
-	
-constructor(private window: WindowService) { }
+books: any[] = [];
+timeout= setTimeout(()=>{this.books= this.repository.books}, 3000);
+
+constructor(private window: WindowService, private repository: UserRepositoryService) { }
 
 ngOnInit(): void { 
     this.window.scrollToTop();
+    // this.books= this.repository.books;
   }
 
 }
