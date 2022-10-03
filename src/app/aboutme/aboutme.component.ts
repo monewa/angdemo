7500138ac5ef41a5fede01aef18c112b76391aa4
monewa <107672359+monewa@users.noreply.github.com>
2022-10-03 15:11:33 +0200

    Add files via upload

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -6,14 +6,42 @@ import { WindowService } from '../services/window.service';
 @Component({
   selector: 'app-aboutme',
   templateUrl: './aboutme.component.html',
-  styleUrls: ['./aboutme.component.css', '../app.component.css']
+  styleUrls: ['./aboutme.component.css', '../app.component.css', '../w3.css']
 })
 export class AboutmeComponent implements OnInit {
 
-  constructor(private window:WindowService) { }
+  picStyle1: string = 'display: none';
+  picStyle2: string = 'display: none';
+  picStyle3: string = 'display: block';
+  timeout= setInterval(() => {this.changetoPic1();}, 3000);
+  timeout1= setInterval(() => {this.changeToPic2();}, 6000);
+  timeout2= setInterval(() => {this.changeToPic3();}, 9000);
+
+   constructor(private window:WindowService) { }
+   
+   changetoPic1(){
+    this.picStyle1 = 'display: block';
+    this.picStyle2 = 'display: none';
+    this.picStyle3 = 'display: none';
+   }
+
+   changeToPic2(){
+    this.picStyle1 = 'display: none';
+    this.picStyle2 = 'display: block';
+    this.picStyle3 = 'display: none';
+   }
+
+   changeToPic3(){
+    this.picStyle1 = 'display: none';
+    this.picStyle2 = 'display: none';
+    this.picStyle3 = 'display: block';
+   }
 
   ngOnInit(): void {
     this.window.scrollToTop();
+    this.timeout;
+    this.timeout1;
+    this.timeout2;
 
   }
 
