929dbc6bacbc640679c4f04222edef7d5f67630f
monewa <107672359+monewa@users.noreply.github.com>
2022-11-29 18:45:53 +0200

    Add files via upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { WindowService } from '../services/window.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css', '../app.component.css', '../w3.css']
+  styleUrls: ['./home.component.css', '../app.component.css']
 })
 export class HomeComponent implements OnInit {
 	
