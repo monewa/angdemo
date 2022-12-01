2dfdb9a008ff837ab8063f28bf9645c4e0f7f444
monewa <107672359+monewa@users.noreply.github.com>
2022-12-01 14:13:21 +0200

    Update golf.component.ts

--- src/app/golf/golf.component.ts
+++ src/app/golf/golf.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
   selector: 'app-golf-home',
   templateUrl: './golf.component.html',
   styleUrls: ['./golf.component.css', '../../bootstrap-5.2.3-dist/css/bootstrap.min.css',
+              './bootstrap.min.css'
   ]
 })
 export class GolfComponent implements AfterViewInit {
