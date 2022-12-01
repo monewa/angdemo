d021f531a5edf1f44549eebb163f3dbeca21b9fb
monewa <107672359+monewa@users.noreply.github.com>
2022-12-01 18:14:58 +0200

    Add files via upload

--- src/app/golf/golf.component.ts
+++ src/app/golf/golf.component.ts
@@ -4,27 +4,19 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-golf-home',
   templateUrl: './golf.component.html',
-  styleUrls: ['./golf.component.css',
+  styleUrls: ['./golf.component.css', 
+  './bootstrap.min.css'
   ]
 })
+
 export class GolfComponent implements AfterViewInit {
 
-  element: string= '';
   constructor() { }
 
-  runScripts(){
-    let button= document.createElement('button');
-    button.textContent= 'close'
-  }
 
   ngAfterViewInit(){
   }
   
   ngOnInit(): void {
-    setTimeout(() => {
-      this.runScripts();
-    }, 4000);
-
   }
-
 }
