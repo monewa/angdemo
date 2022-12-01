b874b6759450dcee0b80286301fff6af0bd8ef44
monewa <107672359+monewa@users.noreply.github.com>
2022-12-01 14:59:10 +0200

    Add files via upload

--- src/app/golf/form/form.component.ts
+++ src/app/golf/form/form.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  name: string= '';
+  score: string= '';
+  message: string= ''; 
   constructor() { }
+
+  savePlayer(){
+    this.message= 'player saved'
+  }
 
   ngOnInit(): void {
   }
